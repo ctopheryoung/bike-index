203ee57f2949ff5778bba00ba967b17c3e6046a0
Brad London and Chris Young <ctopheryoung@gmail.com>
2016-03-16 11:57:03 -0700

    add placeholder, format date, and clear previous results on search

--- js/bike-index-interface.js
+++ js/bike-index-interface.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
   $('#bike-search').click(function() {
+
+    $('#results').empty();
+
     var per_page = "&per_page=" + $('#per_page').val();
     var colors = "&colors=" + $('#colors').val();
     var proximity = "&proximity=" + $('#proximity').val();
+
     $.get("https://bikeindex.org/api/v2/bikes_search/stolen?page=1" + colors + per_page + proximity).then(function(response){
       response.bikes.forEach(function(bike){
-        $('#results').append("bike title:" + bike.title + "<br>");
+        if(bike.thumb !== null){
+          $('#results').append("<img src='" + bike.thumb + "'>");
+        } else {
+          $('#results').append("<img src='./../../img/not-bike.png'>");
+        }
+        $('#results').append("<h3>" + bike.title + "</h3><p>Stolen Location: " + bike.stolen_location + "</p> <p>Date Stolen: " + moment(bike.date_stolen, "X").format("dddd, MMMM, Do, YYYY") + "</p>");
       });
     }).fail(function(error) {
       $('#results').text(error.responseJSON.message);
