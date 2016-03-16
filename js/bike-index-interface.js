48c4152faeae5b4dabc8ccc1a6e88aab78b1126e
Brad London and Chris Young <student@epicodus.com>
2016-03-16 10:24:51 -0700

    add logic to append results to index.html

--- js/bike-index-interface.js
+++ js/bike-index-interface.js
@@ -3,11 +3,10 @@ $(document).ready(function() {
     var per_page = "&per_page=" + $('#per_page').val();
     var colors = "&colors=" + $('#colors').val();
     var proximity = "&proximity=" + $('#proximity').val();
-
-    console.log("https://bikeindex.org/api/v2/bikes_search/stolen?page=1" + colors + per_page + proximity);
-
     $.get("https://bikeindex.org/api/v2/bikes_search/stolen?page=1" + colors + per_page + proximity).then(function(response){
-      console.log(response);
+      response.bikes.forEach(function(bike){
+        $('#results').append("bike title:" + bike.title + "<br>");
+      });
     }).fail(function(error) {
       $('#results').text(error.responseJSON.message);
     });
